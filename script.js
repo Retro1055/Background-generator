285c95f960e4d3db920f5d962ecac00b21782918
Retro1055 <42443575+Retro1055@users.noreply.github.com>
2020-12-19 18:18:42 +0530

    error fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var body = document.querySelector("body");
 console.log(body);
-const setgradient=() -> {
+function setgradient() {
 	body.style.background = "linear-gradient(to right, "+
 	color1.value+","+
 	color2.value+")";
